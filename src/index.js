1b0a84c10ee960476f78950780fe696c49cb2d2a
uae <aniekaudo@yahoo.com>
2022-01-17 13:36:06 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import './style.css';
+import { loadList, addToDo } from './app.js';
+
+const input = document.querySelector('.input-field');
+const addInput = document.querySelector('.fa-arrow-left');
+const refresh = document.querySelector('.fa-refresh');
+
+let LIST;
+let id;
+const data = localStorage.getItem('TODO');
+
+if (data) {
+  LIST = JSON.parse(data);
+  id = LIST.length; // set the id to the last one in the list
+  loadList(LIST); // load the list to the user interface
+} else {
+  // if data isn't empty
+  LIST = [];
+  id = 0;
+}
+refresh.addEventListener('click', () => {
+  localStorage.reload();
+});
+
+const pushToDo = () => {
+  const data = localStorage.getItem('TODO');
+
+  if (data) {
+    LIST = JSON.parse(data);
+    id = LIST.length; // set the id to the last one in the list
+    loadList(LIST); // load the list to the user interface
+  } else {
+    // if data isn't empty
+    LIST = [];
+    id = 0;
+  }
+  const toDo = input.value;
+
+  // if the input isn't empty
+  if (toDo) {
+    addToDo(toDo, id, false, false);
+
+    LIST.push({
+      name: toDo,
+      index: id,
+      done: false,
+      trash: false,
+    });
+    loadList(LIST);
+    // add item to localstorage ( this code must be added where the LIST array is updated)
+    localStorage.setItem('TODO', JSON.stringify(LIST));
+
+    id += 1;
+  }
+  input.value = '';
+};
+addInput.addEventListener('click', pushToDo);
+
+document.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    pushToDo();
+  }
+});
